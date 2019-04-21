ac8e61a3b26808a1560bf480f1804097ceb35a7e
Dmitry <dimfloyd@gmail.com>
2019-04-21 15:29:46 +0300

    Add sorting direction option in functions

--- Source.cpp
+++ Source.cpp
@@ -4,136 +4,162 @@
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 
-void shellsSort(vector<T> &v)
+void printVector(vector<T> &v)
 {
-	for (unsigned gap = v.size() / 2; gap > 0; gap /= 2)
-	{
-		for (unsigned i = gap; i < v.size(); i++) {
-
-			for (int j = i; j >= gap && v[j - gap] > v[j]; j-=gap)
-			{
-				swap(v.at(j - 1), v.at(j));
-			}
-		}
-	}
+	for (auto i : v)
+		cout << i;
+	cout << endl;
 }
 
 
-template <typename T>
-void insertionSort(vector<T> &v)
-{
-	for (unsigned i = 1; i < v.size(); i++) {
-		
-		for (int j = i; j > 0 && v[j - 1] > v[j]; j--)
-		{
-			swap(v.at(j - 1), v.at(j));
-		}
-	}
-}
+template<typename T>
 
-template <typename T>
-void bubbleSort(vector<T> &v)
+void bubbleSort(vector<T> &v, bool increas = true)
 {
+	cout << "bubble sort" << endl;
+
 	bool needSort = true;
+
 	while (needSort)
 	{
 		needSort = false;
-		for (unsigned i = 0; i < v.size()-1; i++)
+		for (int i = 0; i < v.size() - 1; i++)
 		{
-			if (v.at(i) > v.at(i + 1))
+			if (increas)
 			{
-				int buf = v.at(i);
-				v[i] = v.at(i + 1);
-				v[i + 1] = buf;
-				needSort = true;
+				if (v.at(i + 1) < v.at(i)) {
+					swap(v.at(i), v.at(i + 1));
+					needSort = true;
+				}
+			}
+			else
+			{
+				if (v.at(i + 1) > v.at(i)) {
+					swap(v.at(i), v.at(i + 1));
+					needSort = true;
+				}
+			}
+
+		}
+	}
+}
+
+template <typename T>
+
+void insertion(vector<T> &v, bool increas = true)
+{
+	cout << "insertion sort" << endl;
+
+	for (unsigned i = 1; i < v.size(); i++)
+	{
+		if (increas) {
+			for (unsigned j = i; j >= 1 && v[j] < v[j - 1]; --j)
+				swap(v.at(j), v.at(j - 1));
+		}
+		else
+		{
+			for (unsigned j = i; j >= 1 && v[j] > v[j - 1]; --j)
+				swap(v.at(j), v.at(j - 1));
+		}
+	}
+}
+
+
+template <typename T>
+
+void shellsort(vector<T> &v, bool increas = true)
+{
+	cout << "shellsort sort" << endl;
+
+	for (unsigned gap = v.size() / 2; gap >= 1; gap /= 2)
+	{
+		for (unsigned i = gap; i < v.size(); i++)
+		{
+			if (increas)
+			{
+				for (unsigned j = i; j >= gap && v[j] < v[j - gap]; j -= gap)
+					swap(v[j], v[j - 1]);
+			}
+			else
+			{
+				for (unsigned j = i; j >= gap && v[j] > v[j - gap]; j -= gap)
+					swap(v[j], v[j - 1]);
 			}
 		}
 	}
 }
 
 template <typename T>
-void sort(vector<T> &v, size_t left, size_t right, size_t mid);
-template <typename T>
-void mergeSort(vector<T> &v, size_t left, size_t right)
-{
-	if (left < right) 
-	{
+void merge(vector<T> &v, size_t left, size_t right, size_t mid, bool increas);
 
-		size_t mid = (right + left) / 2;
-		mergeSort(v, left, mid);
-		mergeSort(v, mid + 1, right);
-		sort(v, left, right, mid);
-	}
+template <typename T>
+
+void mergesort(vector<T> &v, rsize_t left, size_t right, bool increas = true)
+{
+	cout << "mergesort sort" << endl;
+
+	if (v.size() == 1 || left == right) return;
+	size_t mid = (left + right) / 2;
+	mergesort(v, left, mid, increas);
+	mergesort(v, mid + 1, right, increas);
+	merge(v, left, right, mid, increas);
 }
 template <typename T>
-void sort(vector<T> &v, size_t left, size_t right, size_t mid)
+void merge(vector<T> &v, size_t left, size_t right, size_t mid, bool increas)
 {
-	size_t l = left;
-	size_t r = mid+1;
 	vector<T> tmp;
+	size_t l = left;
+	size_t r = mid + 1;
 
 	while (l <= mid && r <= right)
 	{
-		if (v.at(l) <= v.at(r))
-		{
-			tmp.push_back(v.at(l));
-			l++;
-		}
+		bool compare = increas ? v.at(l) < v.at(r) : v.at(l) > v.at(r);
+		if (compare)
+			tmp.push_back(v.at(l++));
 		else
-		{
-			tmp.push_back(v.at(r));
-			r++;
-		}
+			tmp.push_back(v.at(r++));
+
 	}
+
 	while (l <= mid)
-	{
-		tmp.push_back(v.at(l));
-		l++;
-	}
+		tmp.push_back(v.at(l++));
 
 	while (r <= right)
-	{
-		tmp.push_back(v.at(r));
-		r++;
-	}
+		tmp.push_back(v.at(r++));
 
-	size_t i = left;
-	while (!tmp.empty()) {
-		v[i++] = tmp.front();
+	l = left;
+	while (!tmp.empty())
+	{
+		v[l++] = tmp.front();
 		tmp.erase(tmp.begin());
 	}
-	
+
 }
+
 
 int main()
 {
-//vector
+// preparing vector
 	vector<int> v;
-// vector size
-	size_t size = 5000;
-// fill vector
+	size_t size = 10;
 	for (unsigned i = 0; i < size; i++)
+	{
 		v.push_back(i);
-// shuffling vector data		
-	random_shuffle( v.begin(), v.end());
+	}
 
-	for (auto i : v)
-		cout << i << " ";
-	cout << endl;
+	random_shuffle(v.begin(), v.end());
+	printVector(v);
 
-// choose sorting
+// sorting
 
-	mergeSort(v, 0, size - 1);
 	//bubbleSort(v);
-	//insertionSort(v);
-	//shellsSort(v);
+	//insertion(v);
+	//shellsort(v);
+	//mergesort(v, 0, v.size()-1);
 
-// results	
-	for (auto i : v)
-		cout << i << " ";
-	cout << endl;
+	printVector(v);
 
 	system("PAUSE");
 	return 0;
