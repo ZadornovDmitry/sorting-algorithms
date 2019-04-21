b1ab681d3ceea6479a29773774807b2331ea8499
Dmitry <dimfloyd@gmail.com>
2019-04-21 13:06:06 +0300

    add sorting alg.: bubble, insertion, merge and Shellsort

---
+++ Source.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+void shellsSort(vector<int> &v)
+{
+	for (unsigned gap = v.size() / 2; gap > 0; gap /= 2)
+	{
+		for (unsigned i = gap; i < v.size(); i++) {
+
+			for (int j = i; j >= gap && v[j - gap] > v[j]; j-=gap)
+			{
+				swap(v.at(j - 1), v.at(j));
+			}
+		}
+	}
+}
+void insertionSort(vector<int> &v)
+{
+	for (unsigned i = 1; i < v.size(); i++) {
+		
+		for (int j = i; j > 0 && v[j - 1] > v[j]; j--)
+		{
+			swap(v.at(j - 1), v.at(j));
+		}
+	}
+}
+
+void bubbleSort(vector<int> &v)
+{
+	bool needSort = true;
+	while (needSort)
+	{
+		needSort = false;
+		for (unsigned i = 0; i < v.size()-1; i++)
+		{
+			if (v.at(i) > v.at(i + 1))
+			{
+				int buf = v.at(i);
+				v[i] = v.at(i + 1);
+				v[i + 1] = buf;
+				needSort = true;
+			}
+		}
+	}
+}
+
+
+void sort(vector<int> &v, size_t left, size_t right, size_t mid);
+void mergeSort(vector<int> &v, size_t left, size_t right)
+{
+	if (left < right) 
+	{
+
+		size_t mid = (right + left) / 2;
+		mergeSort(v, left, mid);
+		mergeSort(v, mid + 1, right);
+		sort(v, left, right, mid);
+	}
+}
+
+void sort(vector<int> &v, size_t left, size_t right, size_t mid)
+{
+	size_t l = left;
+	size_t r = mid+1;
+	vector<int> tmp;
+
+	while (l <= mid && r <= right)
+	{
+		if (v.at(l) <= v.at(r))
+		{
+			tmp.push_back(v.at(l));
+			l++;
+		}
+		else
+		{
+			tmp.push_back(v.at(r));
+			r++;
+		}
+	}
+	while (l <= mid)
+	{
+		tmp.push_back(v.at(l));
+		l++;
+	}
+
+	while (r <= right)
+	{
+		tmp.push_back(v.at(r));
+		r++;
+	}
+
+	size_t i = left;
+	while (!tmp.empty()) {
+		v[i++] = tmp.front();
+		tmp.erase(tmp.begin());
+	}
+	
+}
+
+int main()
+{
+
+	vector<int> v;
+	size_t size = 5000;
+
+	for (unsigned i = 0; i < size; i++)
+		v.push_back(i);
+	random_shuffle( v.begin(), v.end());
+
+	for (auto i : v)
+		cout << i << " ";
+	cout << endl;
+	//mergeSort(v, 0, size - 1);
+	//bubbleSort(v);
+	//insertionSort(v);
+	//shellsSort(v);
+	for (auto i : v)
+		cout << i << " ";
+	cout << endl;
+
+	system("PAUSE");
+	return 0;
+}
